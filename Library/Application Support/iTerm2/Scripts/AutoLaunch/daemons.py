1d1271d5066bc54fe4fbb0679ec7ffe596d765ed
fredcallaway <fredc@llaway.com>
2023-03-29 10:41:18 -0700

    iterm command to copy current line

--- Library/Application Support/iTerm2/Scripts/AutoLaunch/daemons.py
+++ Library/Application Support/iTerm2/Scripts/AutoLaunch/daemons.py
@@ -2,6 +2,9 @@
 
 import asyncio
 import iterm2
+from iterm2 import Point
+
+import re
 import os
 import sys
 sys.path.insert(0, '/usr/local/lib/miniconda3/lib/python3.7/site-packages/')
@@ -67,6 +70,9 @@ async def singleton(connection, cmd, id_=None, cd=False):
 
         await lg_tab.async_activate()
 
+
+
+
 async def main(connection):
     app = await iterm2.async_get_app(connection)
 
@@ -82,7 +88,6 @@ async def main(connection):
 
     @iterm2.RPC
     async def open_sublime():
-        app = await iterm2.async_get_app(connection)
         window = app.current_window
         project = await window.async_get_variable('user.project')
         if project:
@@ -91,6 +96,53 @@ async def main(connection):
             target = await app.current_window.current_tab.current_session.async_get_variable('path')
         os.system(f'subl "{target}"')
     await open_sublime.async_register(connection)
+
+    @iterm2.RPC
+    async def copy_line():
+        window = app.current_window
+        tab = window.current_tab
+        session = tab.current_session
+        screen = await session.async_get_screen_contents()
+
+        # Select the screen contents. Note that selection "y" coordinates include
+        # overflow, which is lines that have been lost because scrollback history
+        # exceeded its limit. These coordinates are consistent across scroll events,
+        # although they may refer to no-longer-visible lines.
+        line_info = await session.async_get_line_info()
+        (height, history, overflow, first) = (
+            line_info.mutable_area_height,
+            line_info.scrollback_buffer_height,
+            line_info.overflow,
+            line_info.first_visible_line_number)
+
+        # start = iterm2.Point(0, first + overflow + height - 1)
+        cursor = screen.cursor_coord
+
+        coordRange = iterm2.CoordRange(Point(0, cursor.y), Point(0, cursor.y+1))
+        windowedCoordRange = iterm2.WindowedCoordRange(coordRange)
+        sub = iterm2.SubSelection(windowedCoordRange, iterm2.SelectionMode.CHARACTER, False)
+        selection = iterm2.Selection([sub])
+        text = await session.async_get_selection_text(selection)
+        print('before:', text)
+        text = text.strip()
+        regex = '(' + ')|('.join((
+            r'In \[\d+\]: ',
+            r'julia> ',
+            r'.* pkg> ',
+            r'shell> ',
+            r'r\$> ?',
+            r'\#\!> ?',
+            r'❯ ',
+        )) + ')'
+        text = re.sub(regex, '', text)
+        print('after sub:', text)
+        with open('/tmp/clip', 'w') as f:
+            f.write(text)
+
+        # os.system(f'''pbcopy <<'EOF'\n{text}\nEOF''')
+        os.system(f'''cat /tmp/clip | pbcopy''')
+
+    await copy_line.async_register(connection)
 
     async with iterm2.CustomControlSequenceMonitor(connection, "zebra", r'(\w+) ?(.*)') as mon:
         while True:
