ed6536650b788b1ca2abb69546a481465766eca0
fredcallaway <fredc@llaway.com>
2023-04-18 11:05:40 -0400

    lf fzf rg search (first pass)

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -11,5 +11,5 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *) bat -f $1 --line-range :$height;;
+    *) bat -f "$1" --line-range :$height;;
 esac
